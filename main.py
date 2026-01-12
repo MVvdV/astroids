46513477daa61d57da49621008618375bac428f4
MVvdV <mike@vandevliet.eu>
2026-01-12 13:33:07 +1100

    added player object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -9,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    # Player init
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -18,6 +23,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
